05aa1774f90c04b48143a9063a9b9128f8921012
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-20 21:26:58 +0200

    creating custom user model

--- celestial_bay/auth/models.py
+++ celestial_bay/auth/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    # Custom user model using email for authentication instead of username.
+
+    username = None
+    email = models.EmailField(_('email adress'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __repr__(self):
+        return f'{self.id} {self.email}'
