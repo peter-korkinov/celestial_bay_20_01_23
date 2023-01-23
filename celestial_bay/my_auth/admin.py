23c676c50f456fda01ecafc7aa3b872f0672bdbc
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-23 22:48:19 +0200

    changing the default admin panel header

--- celestial_bay/my_auth/admin.py
+++ celestial_bay/my_auth/admin.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext_lazy as _
 from .models import User
 
 
+admin.site.site_header = 'Celestial Bay Admin'
+
 @admin.register(User)
 class UserAdmin(DjangoUserAdmin):
     """Define admin model for custom User model with no email field."""
