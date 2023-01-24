d7c0e8214ab6c3931db328432b03b9ec07bc4fc9
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-24 13:34:07 +0200

    Adding SimpleJWT's token_blacklist app to installed apps.
    Adding the SimpleJWT authentication backend to the REST_FRAMEWORK parameter.
    
    Adjusting the default SimpleJWT access and refresh tokens' lifetimes, and setting refresh token rotation to True.

--- celestial_bay/celestial_bay/settings.py
+++ celestial_bay/celestial_bay/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
 
     'my_auth',
     'galaxies',
@@ -137,3 +138,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+}
