8271baa8b164b5893e688295de62da6f0fcdd223
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-24 13:22:57 +0200

    creating endpoints for login, refreshing access token, registering a new user, logout, changing password of a user, updating user's info

---
+++ celestial_bay/my_auth/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import RegisterView, UpdateUserView, ChangePasswordView, LogoutView
+
+urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(),
+         name='auth_change_password'),
+    path('update_user/<int:pk>/', UpdateUserView.as_view(), name='auth_update_user'),
+]
