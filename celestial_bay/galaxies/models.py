1ae1ae1a0ee3eafd3f5b3fe2f5da227c47a90002
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-25 14:09:57 +0200

    creating Constellation and ConstellationImage models

--- celestial_bay/galaxies/models.py
+++ celestial_bay/galaxies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from versatileimagefield.fields import VersatileImageField, PPOIField
 
-# Create your models here.
+from my_auth.models import User
+
+
+class Constellation(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    abbreviation = models.CharField(max_length=3)
+    area_in_sq_deg = models.FloatField()
+
+    def __str__(self):
+        return f'{self.pk} - {self.name} ({self.abbreviation})'
+
+
+class ConstellationImage(models.Model):
+    constellation = models.ForeignKey(
+        Constellation,
+        on_delete=models.CASCADE,
+        related_name='images'
+    )
+    image = VersatileImageField(
+        'Image',
+        upload_to='images/',
+        ppoi_field='image_ppoi'
+    )
+    image_ppoi = PPOIField()
+
+    def __str__(self):
+        return f'{self.pk} pic of constellation {self.constellation.name}'
