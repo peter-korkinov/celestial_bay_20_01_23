62059103b97b280d6b8bc704d57225b775b10829
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-25 14:14:00 +0200

    registering the Constellation, ConstellationImage, Galaxy, GalaxyImage, Post, PostImage and Comment models with admin

--- celestial_bay/galaxies/admin.py
+++ celestial_bay/galaxies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Constellation, ConstellationImage, Galaxy, GalaxyImage, Post, PostImage, Comment
 
-# Register your models here.
+admin.site.site_header = 'Celestial Bay Admin'
+
+
+@admin.register(Galaxy)
+class GalaxyAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    list_filter = ('galaxy_type', 'constellation')
+
+
+admin.site.register(Constellation)
+admin.site.register(ConstellationImage)
+admin.site.register(GalaxyImage)
+admin.site.register(Post)
+admin.site.register(PostImage)
+admin.site.register(Comment)
