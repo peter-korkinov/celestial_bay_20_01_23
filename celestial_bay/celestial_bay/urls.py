68dc5aac46462eb1afd00ea1b7f083d5a4c650a4
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-25 14:25:13 +0200

    registering the galaxy app endpoints with DefaultRouter and including them in the urlpatterns

--- celestial_bay/celestial_bay/urls.py
+++ celestial_bay/celestial_bay/urls.py
@@ -1,29 +1,28 @@
-"""celestial_bay URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# for django dev server to serve media
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
+from rest_framework.routers import DefaultRouter
+
+from galaxies.views import ConstellationViewSet, ConstellationImageViewSet,\
+    GalaxyViewSet, PostViewSet, GalaxyImageViewSet, PostImageViewSet
+
+router = DefaultRouter()
+router.register(r'constellations', ConstellationViewSet, basename='Constellations')
+router.register(r'galaxies', GalaxyViewSet, basename='Galaxies')
+router.register(r'posts', PostViewSet, basename='Posts')
+router.register(r'constellation_images', ConstellationImageViewSet, basename='Constellation Images')
+router.register(r'galaxy_images', GalaxyImageViewSet, basename='Galaxy Images')
+router.register(r'post_images', PostImageViewSet, basename='Post Images')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('my_auth.urls')),
+    path('', include(router.urls)),
 ]
 
 # for django dev server to serve media
