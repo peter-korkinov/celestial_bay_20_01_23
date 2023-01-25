41ac84137496ff2c64990f6f6473789620327407
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-25 14:23:00 +0200

    creating ConstellationViewSet and ConstellationImageViewSet

--- celestial_bay/galaxies/views.py
+++ celestial_bay/galaxies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from rest_flex_fields import is_expanded
+from rest_flex_fields.views import FlexFieldsMixin, FlexFieldsModelViewSet
+
+from .serializers import ConstellationSerializer, ConstellationImageSerializer, \
+    GalaxySerializer, GalaxyImageSerializer, PostSerializer, PostImageSerializer,\
+    CommentSerializer
+from .models import Constellation, ConstellationImage, Galaxy, GalaxyImage,\
+    Post, PostImage, Comment
+
+
+class ConstellationViewSet(FlexFieldsMixin, ReadOnlyModelViewSet):
+    serializer_class = ConstellationSerializer
+    permit_list_expands = ['galaxies']
+
+    def get_queryset(self):
+        queryset = Constellation.objects.all()
+
+        if is_expanded(self.request, 'galaxies'):
+            queryset = queryset.prefetch_related('galaxies')
+
+        return queryset
+
+
+class ConstellationImageViewSet(FlexFieldsMixin, ReadOnlyModelViewSet):
+    serializer_class = ConstellationImageSerializer
+    queryset = ConstellationImage.objects.all()
