e885b5b867b0b55113a1a9fcf312bd5bc4e47aac
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-25 14:17:17 +0200

    creating GalaxySerializer and GalaxyImageSerializer

--- celestial_bay/galaxies/serializers.py
+++ celestial_bay/galaxies/serializers.py
@@ -17,3 +17,21 @@ class ConstellationImageSerializer(FlexFieldsModelSerializer):
     class Meta:
         model = ConstellationImage
         fields = ['pk', 'constellation', 'image']
+
+
+class GalaxySerializer(FlexFieldsModelSerializer):
+    class Meta:
+        model = Galaxy
+        fields = ['pk', 'name', 'name_origin', 'notes', 'galaxy_type', 'distance',
+                  'apparent_magnitude', 'size', 'owner', 'constellation']
+        expandable_fields = {
+            'images': ('galaxies.GalaxyImageSerializer', {'many': True}),
+        }
+
+
+class GalaxyImageSerializer(FlexFieldsModelSerializer):
+    image = VersatileImageFieldSerializer(sizes='image_headshot')
+
+    class Meta:
+        model = GalaxyImage
+        fields = ['pk', 'galaxy', 'image']
