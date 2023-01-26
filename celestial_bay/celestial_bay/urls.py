a504fd2c472bd2ab74ea19375df9f33e04433217
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-26 21:25:12 +0200

    endpoints for OpenAPI documentation

--- celestial_bay/celestial_bay/urls.py
+++ celestial_bay/celestial_bay/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from rest_framework.routers import DefaultRouter
 
@@ -15,7 +16,8 @@ router.register(r'constellations', ConstellationViewSet, basename='Constellation
 router.register(r'galaxies', GalaxyViewSet, basename='Galaxies')
 router.register(r'posts', PostViewSet, basename='Posts')
 router.register(r'comments', CommentViewSet, basename='Comments')
-router.register(r'constellation_images', ConstellationImageViewSet, basename='Constellation Images')
+router.register(r'constellation_images',
+                ConstellationImageViewSet, basename='Constellation Images')
 router.register(r'galaxy_images', GalaxyImageViewSet, basename='Galaxy Images')
 router.register(r'post_images', PostImageViewSet, basename='Post Images')
 
@@ -24,6 +26,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('my_auth.urls')),
     path('', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 # for django dev server to serve media
