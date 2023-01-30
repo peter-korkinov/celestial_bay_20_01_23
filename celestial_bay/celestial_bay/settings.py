cc998de5870b1a1960c1b6b924308d7cf5207158
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-01-31 00:09:40 +0200

    adding DEFAULT_PAGINATION_CLASS to the REST_FRAMEWORK param

--- celestial_bay/celestial_bay/settings.py
+++ celestial_bay/celestial_bay/settings.py
@@ -149,9 +149,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': [
-        'django_filters.rest_framework.DjangoFilterBackend'
+        'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 }
 
 
