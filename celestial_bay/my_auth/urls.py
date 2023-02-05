18d1cbb4b89c261d137d9f068ce5b36edc617695
Peter Korkinov <73311957+peter-korkinov@users.noreply.github.com>
2023-02-05 04:04:35 +0200

    adding LoginView to urlpatterns and two minor fixes

--- celestial_bay/my_auth/urls.py
+++ celestial_bay/my_auth/urls.py
@@ -3,16 +3,16 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import RegisterView, UpdateUserView, ChangePasswordView, LogoutView,\
-    UserView
+    UserView, LoginView
 
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/', LoginView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
-    path('change_password/<int:pk>/', ChangePasswordView.as_view(),
+    path('change_password/<uuid:pk>/', ChangePasswordView.as_view(),
          name='auth_change_password'),
-    path('update_user/<int:pk>/', UpdateUserView.as_view(), name='auth_update_user'),
+    path('update_user/<uuid:pk>/', UpdateUserView.as_view(), name='auth_update_user'),
     path('users/<uuid:pk>/', UserView.as_view(), name='auth_users'),
 ]
